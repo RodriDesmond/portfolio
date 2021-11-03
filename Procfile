75513f3abef009d01510aaeee49ad79872e8bd8b
Rodrigo Andrés Costa <c.rodri92@gmail.com>
2021-11-03 12:54:39 -0300

    fixed deploy

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
-web: gunicorn portfolio.wsgi:application --log-file - --log-level debug
-python manage.py collectstatic --noinput
-manage.py migrate
+release: python manage.py migrate --run-syncdb
+web gunicorn portfolio.wsgi:application --log-file -
--- apps/servicio_tecnico/urls.py
+++ apps/servicio_tecnico/urls.py
@@ -1,7 +1,9 @@
 from apps.servicio_tecnico.views import home, presupuesto, trabajos
 from django.urls.conf import path
 from django.urls.conf import re_path
+from django.conf import settings
 from django.views.generic.base import RedirectView
+from django.conf.urls.static import static
 
 favicon_view = RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)
 
@@ -12,5 +14,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('api/trabajos',trabajos,name="trabajos"),
     path('servicio_tecnico/presupuesto',presupuesto,name="presupuesto"),
-    path('favicon.ico', RedirectView.as_view(url=('/static/img/favicon.ico')))
-]
+    #FavIcon    
+    re_path(r'^favicon\.ico$', favicon_view),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
