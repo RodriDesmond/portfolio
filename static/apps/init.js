6cb136919b7c6077183b2a11008830f14829288f
Rodrigo Andrés Costa <c.rodri92@gmail.com>
2021-11-03 11:13:42 -0300

    working on css styling

--- static/apps/init.js
+++ static/apps/init.js
@@ -3,9 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems);
     var elems2 = document.querySelectorAll('.parallax');
     var instances2 = M.Parallax.init(elems2);
-    var elems3 = document.querySelectorAll('.carousel');
-    var instances3 = M.Carousel.init(elems3,{
-        duration:50,
-        indicators:true
-    });
+    var elems3 = document.querySelectorAll('.slider');
+    var instances3 = M.Slider.init(elems3);
 });
