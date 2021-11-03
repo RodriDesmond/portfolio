939e3e283687f3a2ee710386a46ba265e9b9b349
Rodrigo Andrés Costa <c.rodri92@gmail.com>
2021-11-03 12:32:56 -0300

    Update urls

--- apps/servicio_tecnico/urls.py
+++ apps/servicio_tecnico/urls.py
@@ -1,5 +1,9 @@
 from apps.servicio_tecnico.views import home, presupuesto, trabajos
 from django.urls.conf import path
+from django.urls.conf import re_path
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)
 
 
 app_name = 'apps.servicio_tecnico'
@@ -8,4 +12,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('api/trabajos',trabajos,name="trabajos"),
     path('servicio_tecnico/presupuesto',presupuesto,name="presupuesto"),
+    re_path(r'^favicon\.ico$', favicon_view),
 ]
