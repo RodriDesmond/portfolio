c4109d19d4418b0a76517c8bc0f329f8f35cdc03
RodriDesmond <c.rodri92@gmail.com>
2021-11-09 17:07:28 -0300

    Change index.html
    Working on content and style

--- static/apps/init.js
+++ static/apps/init.js
@@ -5,4 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances2 = M.Parallax.init(elems2);
     var elems3 = document.querySelectorAll('.slider');
     var instances3 = M.Slider.init(elems3);
+    var elems4 = document.querySelectorAll('.carousel');
+    var instances4 = M.Carousel.init(elems4, {
+        indicators: true,
+
+    });
 });
